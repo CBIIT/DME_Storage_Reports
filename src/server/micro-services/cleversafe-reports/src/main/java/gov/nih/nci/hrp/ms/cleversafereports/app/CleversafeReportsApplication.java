676af03fa1c4b60c2cb9d60c065a7657925335d1
Eran Rosenberg <eran.rosenberg@nih.gov>
2018-08-28 19:55:33 -0700

    Implemented the summary report using generated model and DTO

--- src/server/micro-services/cleversafe-reports/src/main/java/gov/nih/nci/hrp/ms/cleversafereports/app/CleversafeReportsApplication.java
+++ src/server/micro-services/cleversafe-reports/src/main/java/gov/nih/nci/hrp/ms/cleversafereports/app/CleversafeReportsApplication.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan({"gov.nih.nci.hrp.*"})
-class CleversafeReportsApplication {
+public class CleversafeReportsApplication {
 
     // ---------------------------------------------------------------------//
     // Methods
