d2cdb11b0e5af161826e6ef46d266b44fb072afa
Eran Rosenberg <eran.rosenberg@nih.gov>
2021-02-22 14:52:30 -0500

    Upgraded to Angular CLI 11.2.1

--- src/ui/src/app/components/vault-summary-report/vault-summary-report.component.spec.ts
+++ src/ui/src/app/components/vault-summary-report/vault-summary-report.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VaultSummaryReportComponent } from './vault-summary-report.component';
 
@@ -6,7 +6,7 @@ describe('VaultSummaryReportComponent', () => {
   let component: VaultSummaryReportComponent;
   let fixture: ComponentFixture<VaultSummaryReportComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VaultSummaryReportComponent ]
     })
